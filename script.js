637ef17a610cd4e44dd342026b9fcdae855d3e0f
lindabo <98030875+lindabo@users.noreply.github.com>
2022-01-25 15:42:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,52 @@ window.onload = () => {
 function staticLoadPlaces() {
     return [
         {
-            name: 'Magnemite',
+            name: 'Magnemite0',
             location: {
                 lat: 48.095264,
                 lng: 11.525760,
+            }
+        },
+        {
+            name: 'Magnemite1',
+            location: {
+                lat: 48.095622,
+                lng: 11.525775,
+            }
+        },
+        {
+            name: 'Magnemite2',
+            location: {
+                lat: 48.095545,
+                lng: 11.525857,
+            }
+        },
+        {
+            name: 'Magnemite3',
+            location: {
+                lat: 48.095563,
+                lng: 11.525740,
+            }
+        },
+        {
+            name: 'Magnemite4',
+            location: {
+                lat: 48.095609,
+                lng: 11.525337,
+            }
+        },
+        {
+            name: 'Magnemite5',
+            location: {
+                lat: 48.095558,
+                lng: 11.525398,
+            }
+        },
+        {
+            name: 'Magnemite6',
+            location: {
+                lat: 48.095644,
+                lng: 11.525756,
             }
         },
     ];
